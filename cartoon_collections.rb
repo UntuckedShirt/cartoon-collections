8436a492367056eeeb608707afbd94ded396103a
Nathan Miller <natem31190@gmail.com>
2020-12-18 19:34:39 -0500

    commit tree d48b7eba60ba0694621da090764d1723ce12ff30

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,37 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+dwarves = %w[
+  Dopey 
+  Doc 
+  Bashful
+  Grumpy
+  Sneezy
+  Sleepy
+  Happy
+]
+
+def roll_call_dwarves(dwarves)
+    dwarves.each_with_index do |dwarf, i|
+      puts "#{i + 1}. #{dwarf}"
+    end
+  end
+  
+
+
+def summon_captain_planet(planeteers)
+      planeteers.collect {|planeteer| planeteer = planeteer.capitalize! + '!'}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+  
+
+
+def long_planeteer_calls(summon)
+          summon.any?{|summon| summon.length > 4}
+  
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(fancy)
+ 
   cheese_types = ["cheddar", "gouda", "camembert"]
+            fancy.find do |maybe_fancy| cheese_types.include?(maybe_fancy) #method that accepts argument
+  end
 end
